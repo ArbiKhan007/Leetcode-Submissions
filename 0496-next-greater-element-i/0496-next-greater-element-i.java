7651f047fe231d933e22ff50144832e9447f7b08
Shamsuzzoha <33594007+ArbiKhan007@users.noreply.github.com>
2026-02-02 11:27:16 +0530

    Time: 5 ms (17.7%), Space: 45.5 MB (30.47%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,26 +1,41 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map=new HashMap<> ();
-        int[] res=new int[nums1.length];
+        Stack<Integer> st=new Stack<> ();
+
+        int m=nums1.length;
+        int n=nums2.length;
+        int[] res=new int[m];
+        int[] nextGr=new int[n];
         int idx=0;
 
-        for(int i=0;i<nums1.length;i++) res[i]=-1;
-
-        for(int i=0;i<nums2.length;i++){
+        for(int i=0;i<n;i++){
             map.put(nums2[i], i);
         }
 
-        for(int num:nums1){
-            int j=map.get(num)+1;
-            System.out.println(j);
-            for(;j<nums2.length;j++){
-                if(nums2[j]>num){
-                    res[idx]=nums2[j];
-                    break;
-                }
+        for(int i=n-1;i>=0;i--){
+            if(st.isEmpty()){
+                nextGr[i]=-1;
             }
 
-            idx++;
+            if(!st.isEmpty()&&st.peek()<nums2[i]){
+                // pop all smaller elements
+                while (!st.isEmpty() && st.peek() <= nums2[i]) {
+                    st.pop();
+                }
+
+                nextGr[i] = st.isEmpty() ? -1 : st.peek();
+            }
+
+            if(!st.isEmpty()&&st.peek()>nums2[i]){
+                nextGr[i]=st.peek();
+            }
+
+            st.push(nums2[i]);
+        }
+
+        for(int i=0;i<m;i++){
+            res[idx++]=nextGr[map.get(nums1[i])];
         }
 
         return res;
